23509ee8eea35a686fb171cbdcfc954b1a24668b
Damien Abeloos <dabeloos@w-r10-p2.s19.be>
2018-08-30 20:01:32 +0200

    ft_list_sort FINAL

--- ex14/ft_list_sort.c
+++ ex14/ft_list_sort.c
@@ -6,7 +6,7 @@
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/30 12:56:36 by dabeloos          #+#    #+#             */
-/*   Updated: 2018/08/30 14:03:48 by dabeloos         ###   ########.fr       */
+/*   Updated: 2018/08/30 20:01:20 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,16 @@
 
 void		ft_list_sort(t_list **begin_list, int (*cmp)())
 {
-	t_list head;
-	t_list previous;
+	t_list *head;
+	t_list *previous;
 
 	if (*begin_list == NULL)
 		return ;
 	previous = *begin_list;
-	head = *begin_list->next;
+	head = (*begin_list)->next;
 	while (head != NULL)
 	{
-		if (cmp((*begin_list)->data, head->data) > 0)
+		if (cmp(previous->data, head->data) > 0)
 		{
 			previous->next = head->next;
 			head->next = *begin_list;
