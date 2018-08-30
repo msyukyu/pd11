078b0006d4bc6152751488978d6201800ccc6976
Damien Abeloos <dabeloos@w-r10-p2.s19.be>
2018-08-30 19:46:10 +0200

    ft_list_remove_if FINAL

--- ex12/ft_list_remove_if.c
+++ ex12/ft_list_remove_if.c
@@ -6,7 +6,7 @@
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/30 12:02:53 by dabeloos          #+#    #+#             */
-/*   Updated: 2018/08/30 12:42:15 by dabeloos         ###   ########.fr       */
+/*   Updated: 2018/08/30 19:45:57 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,6 @@ void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
 				previous->next = head->next;
 			else
 				*begin_list = head->next;
-			free(head->data);
 			free(head);
 			if (previous != NULL)
 				head = previous->next;
