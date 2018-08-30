bc3c37f76bd0dfa04fe13af9a7d1d638e5159f0d
Damien Abeloos <dabeloos@w-r10-p2.s19.be>
2018-08-30 18:50:55 +0200

    ft_list_reverse FINAL

--- ex08/ft_list_reverse.c
+++ ex08/ft_list_reverse.c
@@ -6,7 +6,7 @@
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/27 13:51:25 by dabeloos          #+#    #+#             */
-/*   Updated: 2018/08/30 16:57:55 by dabeloos         ###   ########.fr       */
+/*   Updated: 2018/08/30 18:48:08 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,9 @@ void		ft_list_reverse(t_list **begin_list)
 {
 	t_list	*head;
 
+	if (*begin_list == NULL || (*begin_list)->next == NULL)
+		return ;
 	head = *begin_list;
 	ft_reverse(begin_list, head, head->next);
+	head->next = NULL;
 }
