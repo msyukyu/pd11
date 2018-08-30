df5628ea63bbe78f66faca05a73c544cb939dbed
Damien Abeloos <dabeloos@w-r10-p2.s19.be>
2018-08-30 17:41:46 +0200

    ft_list_last FINAL

--- ex04/ft_list_last.c
+++ ex04/ft_list_last.c
@@ -6,7 +6,7 @@
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/26 19:55:22 by dabeloos          #+#    #+#             */
-/*   Updated: 2018/08/30 11:47:10 by dabeloos         ###   ########.fr       */
+/*   Updated: 2018/08/30 17:41:29 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ t_list		*ft_list_last(t_list *begin_list)
 {
 	t_list	*head;
 
+	if (begin_list == NULL)
+		return (NULL);
 	head = begin_list;
 	while (head->next != NULL)
 		head = head->next;
