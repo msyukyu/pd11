8334469e17b5ef26eaba68fe03a72ac2afccb2ab
Damien Abeloos <dabeloos@w-r10-p2.s19.be>
2018-08-30 18:14:47 +0200

    ft_list_clear FINAL

--- ex06/ft_list_clear.c
+++ ex06/ft_list_clear.c
@@ -6,7 +6,7 @@
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/27 13:27:49 by dabeloos          #+#    #+#             */
-/*   Updated: 2018/08/30 11:47:28 by dabeloos         ###   ########.fr       */
+/*   Updated: 2018/08/30 18:14:36 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,9 @@
 
 void		ft_list_clear(t_list **begin_list)
 {
-	t_list	*head;
-
-	head = *begin_list;
-	if (head->next != NULL)
-		ft_list_clear(t_list &(head->next));
-	head->next = NULL;
+	if ((*begin_list)->next != NULL)
+		ft_list_clear(&((*begin_list)->next));
+	(*begin_list)->next = NULL;
+	free(*begin_list);
+	*begin_list = NULL;
 }
