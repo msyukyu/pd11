6d77569e33dd4d93836a7174d980a01a9f1b8fe1
Damien Abeloos <dabeloos@w-r10-p2.s19.be>
2018-08-30 20:11:40 +0200

    fake reverse fun

--- ex15/ft_list_reverse_fun.c
+++ ex15/ft_list_reverse_fun.c
@@ -5,14 +5,14 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/08/30 14:54:40 by dabeloos          #+#    #+#             */
-/*   Updated: 2018/08/30 16:57:06 by dabeloos         ###   ########.fr       */
+/*   Created: 2018/08/30 20:07:04 by dabeloos          #+#    #+#             */
+/*   Updated: 2018/08/30 20:11:28 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_list.h"
 
-void        ft_reverse(t_list **begin_list, t_list *head, t_list *deep)
+void		ft_reverse_bis(t_list **begin_list, t_list *head, t_list *deep)
 {
 	if (deep->next == NULL)
 		*begin_list = deep;
@@ -21,10 +21,13 @@ void        ft_reverse(t_list **begin_list, t_list *head, t_list *deep)
 	deep->next = head;
 }
 
-void        ft_list_reverse_fun(t_list *begin_list)
+void		ft_list_reverse_fun(t_list *begin_list)
 {
-	t_list  *head;
+	t_list	*head;
 
+	if (begin_list == NULL || begin_list->next == NULL)
+		return ;
 	head = begin_list;
-	ft_reverse(begin_list, head, head->next);
+	ft_reverse_bis(&begin_list, head, head->next);
+	head->next = NULL;
 }
