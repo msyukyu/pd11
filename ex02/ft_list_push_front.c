1341b76a4fda433b0f63d5c26a9151bb6b66c87f
Damien Abeloos <dabeloos@w-r10-p2.s19.be>
2018-08-30 17:27:27 +0200

    ft_list_push_front FINAL

--- ex02/ft_list_push_front.c
+++ ex02/ft_list_push_front.c
@@ -6,7 +6,7 @@
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/26 19:47:28 by dabeloos          #+#    #+#             */
-/*   Updated: 2018/08/30 11:46:42 by dabeloos         ###   ########.fr       */
+/*   Updated: 2018/08/30 17:27:11 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,10 @@
 
 void		ft_list_push_front(t_list **begin_list, void *data)
 {
-	t_list *item;
-	int		i;
-	t_list *head;
+	t_list	*item;
+	t_list	*head;
 
 	item = ft_create_elem(data);
 	item->next = *begin_list;
-	begin_list = &item;
+	*begin_list = item;
 }
