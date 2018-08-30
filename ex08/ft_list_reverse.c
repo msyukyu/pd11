2977f3aab59fd20a5f8b8f16bcbb139074e8d6de
Damien Abeloos <dabeloos@w-r10-p2.s19.be>
2018-08-30 16:58:15 +0200

    ex15 is wrong

--- ex08/ft_list_reverse.c
+++ ex08/ft_list_reverse.c
@@ -6,19 +6,19 @@
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/27 13:51:25 by dabeloos          #+#    #+#             */
-/*   Updated: 2018/08/30 11:33:29 by dabeloos         ###   ########.fr       */
+/*   Updated: 2018/08/30 16:57:55 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_list.h"
 
-void		ft_reverse(t_list **begin_list, t_list **head, t_list **deep)
+void		ft_reverse(t_list **begin_list, t_list *head, t_list *deep)
 {
-	if (*deep->next == NULL)
-		**begin_list = deep;
+	if (deep->next == NULL)
+		*begin_list = deep;
 	else
 		ft_reverse(begin_list, deep, deep->next);
-	*deep->next = *head;
+	deep->next = head;
 }
 
 void		ft_list_reverse(t_list **begin_list)
--- ex15/ft_list_reverse_fun.c
+++ ex15/ft_list_reverse_fun.c
@@ -6,33 +6,25 @@
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/30 14:54:40 by dabeloos          #+#    #+#             */
-/*   Updated: 2018/08/30 15:14:00 by dabeloos         ###   ########.fr       */
+/*   Updated: 2018/08/30 16:57:06 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_list.h"
 
-void        ft_reverse(t_list **begin_list, t_list **head, t_list **deep)
+void        ft_reverse(t_list **begin_list, t_list *head, t_list *deep)
 {
-	if (*deep->next == NULL)
-		**begin_list = deep;
+	if (deep->next == NULL)
+		*begin_list = deep;
 	else
 		ft_reverse(begin_list, deep, deep->next);
-	*deep->next = *head;
+	deep->next = head;
 }
 
-void        ft_list_reverse(t_list **begin_list)
+void        ft_list_reverse_fun(t_list *begin_list)
 {
 	t_list  *head;
 
-	head = *begin_list;
+	head = begin_list;
 	ft_reverse(begin_list, head, head->next);
-}
-
-void	ft_list_reverse_fun(t_list *begin_list)
-{
-	t_list **start;
-	
-	start = &begin_list;
-	ft_list_reverse(start);
 }
