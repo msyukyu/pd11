42d261521ccd688c1e21a37ab99845bb8c6b4ee4
Damien Abeloos <dabeloos@w-r10-p2.s19.be>
2018-08-30 17:57:41 +0200

    ft_list_push_params FINAL

--- ex05/ft_list_push_params.c
+++ ex05/ft_list_push_params.c
@@ -6,7 +6,7 @@
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/26 19:57:37 by dabeloos          #+#    #+#             */
-/*   Updated: 2018/08/30 11:47:19 by dabeloos         ###   ########.fr       */
+/*   Updated: 2018/08/30 17:57:27 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,16 @@
 
 t_list		*ft_list_push_params(int ac, char **av)
 {
-	t_list	first;
-	t_list	second;
+	t_list	*first;
+	t_list	*second;
+	int		i;
 
+	i = ac;
 	if (ac > 1)
-		first = ft_create_elem(av[ac - 1]);
+		first = ft_create_elem(av[i - (ac - 1)]);
 	while (--ac > 1)
 	{
-		second = ft_create_elem(av[ac - 1]);
+		second = ft_create_elem(av[i - (ac - 1)]);
 		second->next = first;
 		first = second;
 	}
