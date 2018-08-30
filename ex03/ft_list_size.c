98c434455a234ff40bedde9a054363de86850ced
Damien Abeloos <dabeloos@w-r10-p2.s19.be>
2018-08-30 17:34:57 +0200

    ft_list_size FINAL

--- ex03/ft_list_size.c
+++ ex03/ft_list_size.c
@@ -6,7 +6,7 @@
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/26 19:48:42 by dabeloos          #+#    #+#             */
-/*   Updated: 2018/08/30 11:46:53 by dabeloos         ###   ########.fr       */
+/*   Updated: 2018/08/30 17:34:43 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ int			ft_list_size(t_list *begin_list)
 	int		i;
 	t_list	*head;
 
-	i = 1;
+	i = 0;
 	head = begin_list;
-	while (head->next != NULL)
+	while (head != NULL)
 	{
 		i++;
 		head = head->next;
